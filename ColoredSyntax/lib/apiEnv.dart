a0501914b1e71a43ea8c7b0d18aef18abcf6ccd5
HALKA9000-NIIP <csta19097@g.nihon-u.ac.jp>
2021-05-23 01:09:08 +0900

    cr: ColoredSyntax package

---
+++ ColoredSyntax/lib/apiEnv.dart
@@ -0,0 +1,10 @@
+import 'package:ColoredSyntax/coloredSyntax.dart';
+import 'package:image/image.dart' as imgLib;
+import 'dart:typed_data';
+import 'dart:html' ;
+extension SyntaxAPI on ColoredSyntax{
+  imgLib.Image imager(this){}
+  Widget flutterDOM(){}
+  Element htmlDOM(){}
+  
+}
